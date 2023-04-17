dbee73d7970615ed2cb78863e5a0dd5099078987
OJEDA ROSAS JONATHAN ARATH <zs20006736@estudiantes.uv.mx>
2023-04-16 20:07:09 -0500

    add login facebook

--- src/App.js
+++ src/App.js
@@ -2,38 +2,72 @@ import React, { Component } from "react";
 import { Switch, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import FacebookLogin from 'react-facebook-login';
 
 import AddClip from "./components/add-clip.component";
 import ClipList from "./components/clip-list.component";
 
 class App extends Component {
+
+  state = {
+    isLoggedIn: false,
+    userID: "",
+    name: "",
+    email: "",
+    picture: "",
+  };
+
+  responseFacebook = (response) => {
+    console.log(response);
+    this.setState({
+      isLoggedIn: true,
+      userID: response.userID,
+      name: response.name,
+      email: response.email,
+      picture: response.picture.data.url,
+    });
+  };
+
   render() {
+    const { isLoggedIn, name } = this.state;
+
     return (
       <div>
-        <nav className="navbar navbar-expand navbar-dark bg-dark">
-          <a href="/clips" className="navbar-brand">
-            Dragon Ball Clips
-          </a>
-          <div className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <Link to={"/clips"} className="nav-link">
-                Clips
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to={"/add"} className="nav-link">
-                Subir
-              </Link>
-            </li>
+        <div>
+          <nav className="navbar navbar-expand navbar-dark bg-dark">
+            <a href="#" className="navbar-brand">
+              Dragon Ball Clips
+            </a>
+            <div className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <Link to={"/clips"} className="nav-link">
+                  Clips
+                </Link>
+              </li>
+              {isLoggedIn && (
+                <li className="nav-item">
+                  <Link to={"/add"} className="nav-link">
+                    Subir
+                  </Link>
+                </li>)}
+            </div>
+            <h2 className="navbar-brand">{isLoggedIn ? name : (
+              <FacebookLogin
+                appId="1373563823219650"
+                autoLoad={false}
+                fields="name,email,picture"
+                onClick={() => console.log("Iniciando sesión con Facebook...")}
+                callback={this.responseFacebook} />
+            )}
+            </h2>
+          </nav>
+          <div className="container mt-3">
+            <h2>Dragon Ball Clips</h2>
+            <Switch>
+              <Route exact path={["/", "/clips"]} component={ClipList} />
+              <Route exact path="/add" component={AddClip} />
+            </Switch>
           </div>
-        </nav>
-
-        <div className="container mt-3">
-          <h2>Dragon Ball Clips</h2>
-          <Switch>
-            <Route exact path={["/", "/clips"]} component={ClipList} />
-            <Route exact path="/add" component={AddClip} />
-          </Switch>
         </div>
       </div>
     );
